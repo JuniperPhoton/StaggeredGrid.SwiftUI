8d03ad9323449e9392d9c43e0a62834c24b44a37
dengweichao <dengweichao@hotmail.com>
2025-01-11 17:51:11 +0800

    Update docs

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
             targets: ["StaggeredGrid"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StaggeredGrid.SwiftUI
 
+A view to layout children in a staggered way.
+
+See the documentation below for more inforamtion.
+
+https://juniperphoton.github.io/StaggeredGrid.SwiftUI/documentation/staggeredgrid/
